e1d5d217fbc80fd9dcfc480300ab004ed6e10980
MuhammadTokyo <richardaveryan@bk.ru>
2024-06-15 20:00:53 +0900

    next

---
+++ nea.jp/top.php
@@ -0,0 +1,405 @@
+<?php require("nav.php");?>
+<link rel="stylesheet" href="css/top.css">
+
+
+<div class="bg-body-secondary w-100">
+    <table class="table table-striped table-hover table-bordered text-center container">
+  <thead class="table-info" >
+    <tr >
+      <th scope="col">番号</th>
+      <th scope="col">面接日 <br>Date</th>
+      <th scope="col">曜日 <br>Day</th>
+      <th scope="col">参加枠 <br>Entry</th>
+      <th scope="col">主な参加企業 <br>Company</th>
+      <th scope="col">業 種 <br>Type of Job</th>
+      <th scope="col">月給  <br>Salary </th>
+      <th scope="col">勤務地 <br>Location</th>
+      <th scope="col">会社概要・仕事内容 <br>Details of Job</th>
+    </tr>
+  </thead>
+  <tbody>
+  <tr class="table-info ">
+      <th>1</th>
+      <td >6月15日</td>
+      <td>土</td>
+      <td>満席 <i class="bi bi-x-lg"></i><i class="bi bi-x-lg"></i></td>
+      <td>製造・加工（靴・カバン）</td>
+      <td>製造・販売</td>
+      <td>20～25</td>
+      <td>埼玉</td>
+      <td>靴（くつ）や鞄（かばん）を製造し加工するお仕事です。</td>
+    </tr>
+
+    <tr class="table-info">
+      <th>2</th>
+      <td >6月16日</td>
+      <td >日</td>
+      <td>満席 <i class="bi bi-x-lg"></i><i class="bi bi-x-lg"></i></td>
+      <td>和風旅館（天然温泉の露天風呂）</td>
+      <td>ホテル（旅館）</td>
+      <td>17～</td>
+      <td>長野</td>
+      <td>和風旅館。天然温泉の露天風呂で見上げる星は最高。</td>
+    </tr>
+
+    <tr class="table-info">
+      <th>3</th>
+      <td >6月17日</td>
+      <td >月</td>
+      <td>満席 <i class="bi bi-x-lg"></i></td>
+      <td>シーサイドホテル</td>
+      <td>ホテル</td>
+      <td>21～24</td>
+      <td>茨城</td>
+      <td>ホテルの仕事です。接客業が大好きな方大歓迎です。 </td>
+    </tr>
+
+    <tr class="table-info">
+      <th>4</th>
+      <td >6月17日</td>
+      <td >月</td>
+      <td>満席 <i class="bi bi-triangle-fill"></i></td>
+      <td>シーサイドホテル</td>
+      <td>ホテル</td>
+      <td>21～24</td>
+      <td>茨城</td>
+      <td>ホテルの仕事です。接客業が大好きな方大歓迎です。 </td>
+    </tr>
+
+    <tr class="table-info">
+      <th>5</th>
+      <td >6月17日</td>
+      <td >月</td>
+      <td>満席 <i class="bi bi-triangle"></i></td>
+      <td>シーサイドホテル</td>
+      <td>ホテル</td>
+      <td>21～24</td>
+      <td>茨城</td>
+      <td>ホテルの仕事です。接客業が大好きな方大歓迎です。 </td>
+    </tr>
+
+    <tr class="table-info">
+      <th>6</th>
+      <td >6月17日</td>
+      <td >月</td>
+      <td>満席 <i class="bi bi-square-fill"></i></td>
+      <td>シーサイドホテル</td>
+      <td>ホテル</td>
+      <td>21～24</td>
+      <td>茨城</td>
+      <td>ホテルの仕事です。接客業が大好きな方大歓迎です。 </td>
+    </tr>
+
+    <tr class="table-info">
+      <th>7</th>
+      <td >6月17日</td>
+      <td >月</td>
+      <td>満席 <i class="bi bi-square"></i></td>
+      <td>シーサイドホテル</td>
+      <td>ホテル</td>
+      <td>21～24</td>
+      <td>茨城</td>
+      <td>ホテルの仕事です。接客業が大好きな方大歓迎です。 </td>
+    </tr>
+
+    <tr class="table-info">
+      <th>3</th>
+      <td >6月17日</td>
+      <td >月</td>
+      <td>満席   </td>
+      <td>シーサイドホテル</td>
+      <td>ホテル</td>
+      <td>21～24</td>
+      <td>茨城</td>
+      <td>ホテルの仕事です。接客業が大好きな方大歓迎です。 </td>
+    </tr>
+
+    <tr class="table-info">
+      <th>3</th>
+      <td >6月17日</td>
+      <td >月</td>
+      <td>満席   </td>
+      <td>シーサイドホテル</td>
+      <td>ホテル</td>
+      <td>21～24</td>
+      <td>茨城</td>
+      <td>ホテルの仕事です。接客業が大好きな方大歓迎です。 </td>
+    </tr>
+
+    <tr class="table-info">
+      <th>3</th>
+      <td >6月17日</td>
+      <td >月</td>
+      <td>満席   </td>
+      <td>シーサイドホテル</td>
+      <td>ホテル</td>
+      <td>21～24</td>
+      <td>茨城</td>
+      <td>ホテルの仕事です。接客業が大好きな方大歓迎です。 </td>
+    </tr>
+
+    <tr class="table-info">
+      <th>3</th>
+      <td >6月17日</td>
+      <td >月</td>
+      <td>満席   </td>
+      <td>シーサイドホテル</td>
+      <td>ホテル</td>
+      <td>21～24</td>
+      <td>茨城</td>
+      <td>ホテルの仕事です。接客業が大好きな方大歓迎です。 </td>
+    </tr>
+
+    <tr class="table-info">
+      <th>3</th>
+      <td >6月17日</td>
+      <td >月</td>
+      <td>満席   </td>
+      <td>シーサイドホテル</td>
+      <td>ホテル</td>
+      <td>21～24</td>
+      <td>茨城</td>
+      <td>ホテルの仕事です。接客業が大好きな方大歓迎です。 </td>
+    </tr>
+
+    <tr class="table-info">
+      <th>3</th>
+      <td >6月17日</td>
+      <td >月</td>
+      <td>満席   </td>
+      <td>シーサイドホテル</td>
+      <td>ホテル</td>
+      <td>21～24</td>
+      <td>茨城</td>
+      <td>ホテルの仕事です。接客業が大好きな方大歓迎です。 </td>
+    </tr>
+
+    <tr class="table-info">
+      <th>3</th>
+      <td >6月17日</td>
+      <td >月</td>
+      <td>満席   </td>
+      <td>シーサイドホテル</td>
+      <td>ホテル</td>
+      <td>21～24</td>
+      <td>茨城</td>
+      <td>ホテルの仕事です。接客業が大好きな方大歓迎です。 </td>
+    </tr>
+
+  </tbody>
+</table
+
+
+</div>
+    
+    </div>
+
+
+
+
+
+
+<!--毎日開催中!-->
+<div class="card border-primary bg-info-subtle mb-3 w-100 p-3">
+<div class="card-header">
+<p class="fs-3 fw-bold text-center rounded-5 rounded-top-0 p-3"><span class="p-3 bg-white bg-opacity-10 border border-info rounded">合同説明会</span></p>
+
+
+</div>
+  <div class="row g-0 p-4">
+    <div class="col-md-11">
+      <div class="card-body">
+        <h3 class="card-title text-primary text-center fw-bold">
+        外国人材と会える <span class="fs-1">「 合同説明会」</span> を <span class="fs-1"> 毎日開催中!</span>
+        </h3>
+        <p class="card-text p-3">全国外国人雇用協会は、採用支援・就活支援の一環として、法人会員と個人会員間の就活上の交流や面接を促すために「合同説明会」を毎日開催しております（求人企業8社1回まで）。参加希望の方は事務局（03-6206-8058）にお問い合わせ下さい。<br>
+日本人の人口は今後50年間で4,600万人減少します。要するにつまり、毎年100万人減っていくのです。この運命は誰にも変えられません。そして、「日本人が毎年100万人減っていく」という事実は、一日当たり3,000 人という就労者が確実に消えていくことを意味しています。つまり、毎日3,000社の日本企業で「人手不足」が発生し続けることになります。<br>
+活力あふれる企業として、将来においても生き残ろうとするのであれば、外国人材を 巧みに活用する「社内インフラ」を社内に構築・運用するしかありません。日本人社員だけで対応 しようとすれば、新しいことに取り組む暇もなく、通常業務に追われるだけの 毎日になります。将来のビジョンを描く経営者であれば、積極的に外国人材を採用 し、最大限に活用する方法を検討しなければならない時代が到来しているのです。<br>
+当協会では、上記の課題に直面している日本企業を支援するため、企業広告サービスを提供しています。貴社の魅力をお知らせする会社概要を制作して効果的な宣伝を行うほか、貴社が参加する「合同説明会」を広報して、多士多才な外国人材に来社していただいています。貴社への入社を希望する参加者と直接会える「合同説明会」は、毎日25~50人が参加するイベントであり、開催回数は1,700回を超えています。.</p>
+<div class="card-footer bg-transparent border-dark  text-center">
+
+<a href="/moushikomi.php">
+<button type="button" class="btn btn-primary btn-lg col-6">参加申込み &nbsp; &nbsp; &nbsp; &nbsp; <i class="bi bi-envelope-arrow-down"></i></button></div>
+</a>
+
+      </div>
+    </div>
+  </div>
+</div>
+
+<!--毎日開催中! end-->
+
+<!--Consultation-->
+
+<div class="bg-body-secondary p-5">
+
+
+            <div class="card bg-danger-subtle bg-danger w-100 m-auto p-5 shadow-lg rounded-top-5">
+            <div class="card-header bg-transparent border-success">
+                <h1 class="fs-1 fw-bold">
+                    VISA
+                </h1>
+            </div>
+            <div class="card-body text-success row">
+                <h5 class="card-title col-12 col-sm-6 m-auto">ビザに関するご相談はこちらへ
+                    <br>
+                    If you have any trouble with your VISA, click here !
+                    <br>
+                    点击这里进行签证咨询
+                    <br>
+                    Bấm vào đây để tư vấn visa</h5>
+       
+                    <a href="moushikomi.php" class="col-12 col-sm-6 btn btn-success m-auto shadow">
+                    <p>お問い合わせ・お申し込み</p>
+                    <p>Application Form 咨询・申请报名</p>
+                    <p>Những vấn đề thắc mắc. Cách thức đăng ký</p>
+                    </a>
+                
+            </div>
+            </div>
+
+            <div class="container d-md-flex justify-content-around mt-5">
+
+            <img src="img/img_fair1.jpg" alt="VISA,仕事、動力" class="img-thumbnail img-fluid shadow col-5 col-md-2">
+            <img src="img/img_fair3.jpg" alt="VISA,仕事、動力" class="img-thumbnail img-fluid shadow col-5 col-md-2">
+            <img src="img/img_kyoka.jpg" alt="VISA,仕事、動力" class="img-thumbnail img-fluid shadow col-5 col-md-2">
+            <img src="img/img_nintei.jpg" alt="VISA,仕事、動力" class="img-thumbnail img-fluid shadow col-5 col-md-2">
+            <img src="img/img_fair2.jpg" alt="VISA,仕事、動力" class="img-thumbnail img-fluid shadow col-5 col-md-2">
+            <img src="img/img_naitei.jpg" alt="VISA,仕事、動力" class="img-thumbnail img-fluid shadow col-5 col-md-2">
+
+            </div>
+
+
+
+
+
+            <div class="card bg-danger-subtle bg-danger w-100 m-auto p-5 shadow-lg rounded-top-5 mt-5">
+            <div class="card-header bg-transparent border-success">
+                <h1 class="fs-1 fw-bold">
+                JOB FAIR
+                </h1>
+            </div>
+            <div class="card-body text-success row">
+                <h5 class="card-title col-12 col-sm-6 m-auto">「就職フェア」に参加したい方はこちらへ
+                    <br>
+                    If you want to participate JOB FAIR, click here !
+                    <br>
+                    如果您想参加“工作展”，请点击这里
+                    <br>
+                    “Triển lãm thủ công” có Rugoo Yusou tham gia, đặc trưng ở Kori</h5>
+       
+                    <a href="moushikomi.php" class="col-12 col-sm-6 btn btn-success m-auto shadow">
+                    <p>お問い合わせ・お申し込み</p>
+                    <p>Application Form 咨询・申请报名</p>
+                    <p>Những vấn đề thắc mắc. Cách thức đăng ký</p>
+                    </a>
+                
+            </div>
+            </div>
+
+            <div class="container m-auto mt-5">
+
+            <img src="img/TimeSchedule.jpg" alt="VISA,仕事、動力" class="img-thumbnail img-fluid shadow col-12">
+            
+
+            </div>
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+</div>
+
+
+
+
+<!--Consultation  end-->
+
+
+
+
+
+
+
+<!--日時的-->
+
+
+
+
+
+
+
+
+
+
+
+<div class="w-100 mt-5 bg-info-subtle">
+
+
+<div class="d-flex justify-content-center">
+<img src="img/topContent/content1.png" alt="仕事、バイト、正社員" class="img-fluid img-thumbnail topConImage">
+<img src="img/topContent/content2.png" alt="仕事、バイト、正社員" class="img-fluid img-thumbnail topConImage">
+<img src="img/topContent/content3.png" alt="仕事、バイト、正社員" class="img-fluid img-thumbnail topConImage">
+<img src="img/topContent/content4.png" alt="仕事、バイト、正社員" class="img-fluid img-thumbnail topConImage">
+</div>
+<div class="d-flex justify-content-center">
+<img src="img/topContent/content5.png" alt="仕事、バイト、正社員" class="img-fluid img-thumbnail topConImage">
+<img src="img/topContent/content6.png" alt="仕事、バイト、正社員" class="img-fluid img-thumbnail topConImage">
+<img src="img/topContent/content7.png" alt="仕事、バイト、正社員" class="img-fluid img-thumbnail topConImage">
+<img src="img/topContent/content8.png" alt="仕事、バイト、正社員" class="img-fluid img-thumbnail topConImage">
+</div>
+<div class="d-flex justify-content-center">
+<img src="img/topContent/content9.png" alt="仕事、バイト、正社員" class="img-fluid img-thumbnail topConImage">
+<img src="img/topContent/content10.png" alt="仕事、バイト、正社員" class="img-fluid img-thumbnail topConImage">
+<img src="img/topContent/content11.png" alt="仕事、バイト、正社員" class="img-fluid img-thumbnail topConImage">
+<img src="img/topContent/content12.png" alt="仕事、バイト、正社員" class="img-fluid img-thumbnail topConImage">
+</div>
+<div class="d-flex justify-content-center">
+<img src="img/topContent/content13.png" alt="仕事、バイト、正社員" class="img-fluid img-thumbnail topConImage">
+<img src="img/topContent/content14.png" alt="仕事、バイト、正社員" class="img-fluid img-thumbnail topConImage">
+<img src="img/topContent/content15.png" alt="仕事、バイト、正社員" class="img-fluid img-thumbnail topConImage">
+<img src="img/topContent/content16.png" alt="仕事、バイト、正社員" class="img-fluid img-thumbnail topConImage">
+</div>
+<div class="d-flex justify-content-center">
+<img src="img/topContent/content17.png" alt="仕事、バイト、正社員" class="img-fluid img-thumbnail topConImage">
+<img src="img/topContent/content18.png" alt="仕事、バイト、正社員" class="img-fluid img-thumbnail topConImage">
+<img src="img/topContent/content19.png" alt="仕事、バイト、正社員" class="img-fluid img-thumbnail topConImage">
+<img src="img/topContent/content20.png" alt="仕事、バイト、正社員" class="img-fluid img-thumbnail topConImage">
+</div>
+<div class="d-flex justify-content-center">
+<img src="img/topContent/content21.png" alt="仕事、バイト、正社員" class="img-fluid img-thumbnail topConImage">
+<img src="img/topContent/content22.png" alt="仕事、バイト、正社員" class="img-fluid img-thumbnail topConImage">
+<img src="img/topContent/content23.png" alt="仕事、バイト、正社員" class="img-fluid img-thumbnail topConImage">
+<img src="img/topContent/content24.png" alt="仕事、バイト、正社員" class="img-fluid img-thumbnail topConImage">
+</div>
+
+
+</div>
+
+
+
+
+
+<!--日時的 end-->
+
+
+
+
+
+
+
+
+
+
+
+
+<?php require("footer.php");?>
